10c3cc583b2d967a8a47dc97551fb3da79031db0
MohitRustagi <mohit.rustagi@live.in>
2020-03-31 16:15:17 +0530

    Delete main.ts

--- main.ts
+++
@@ -1,6 +0,0 @@
-import { LikeComponent } from './likes';
-
-let likesComponent = new LikeComponent(10, true);
-likesComponent.onClick();
-console.log(`Total no of clicks - ${likesComponent.likesCount}`);
-console.log(`Like button selected? - ${likesComponent.isSelected}`);
